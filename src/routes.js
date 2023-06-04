df50e2a9a09c5934482ed5af5b61b57314564731
Satria Berliano <sanozisb@gmail.com>
2023-06-04 20:53:07 +0700

    Delete src directory

--- src/routes.js
+++
@@ -1,6 +0,0 @@
-export const rootPath = "/";
-export const homePath = "/home";
-export const featurePath = "/feature";
-export const facilityPath = "/facility";
-export const buildingPath = "/building";
-export const aboutUsPath = "/about";
