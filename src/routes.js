6be90f04c0bc7d976bcb530f08ebf49c1f425f58
Satria Berliano <sanozisb@gmail.com>
2023-06-05 22:38:46 +0700

    Delete src directory

--- src/routes.js
+++
@@ -1,6 +0,0 @@
-export const rootPath = "/BBPBL-WebGIS";
-export const homePath = "/BBPBL-WebGIS/home";
-export const featurePath = "/BBPBL-WebGIS/feature";
-export const facilityPath = "/BBPBL-WebGIS/facility";
-export const buildingPath = "/BBPBL-WebGIS/building";
-export const aboutUsPath = "/BBPBL-WebGIS/about";
